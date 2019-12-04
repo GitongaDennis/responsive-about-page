6c00b9cb662e47bb4c58a825e8e9742a18a2e6b9
Gitonga Dennis <gitonhadennis36.dg@gmail.com>
2019-12-04 08:37:31 +0300

    too many warnings

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,59 @@
+/* eslint-disable */
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-card">
+    <div class="card-title">
+      <img src="https://i.pinimg.com/originals/d3/69/d9/d369d9056795f553e244da66e8297cca.png" alt="avatar" class="avatar">
+      <div class="title-text-content">
+    <div class="title-text-content">
+      <p>About Us </p>
+      <h2>Tour Kenya</h2>
+    </div>
+      </div>
   </div>
+  <div class="card-content">
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius consequatur, libero, natus,
+       autem hic saepe exercitationem expedita alias corporis quisquam reiciendis ab quaerat
+        rerum itaque animi repudiandae dolor in fugit,
+       ratione inventore molestiae harum accusantium dignissimos. Id praesentium
+        quisquam asperiores ullam quaerat totam, voluptates esse vero repellendus amet, officiis inventore.</p>
+
+      <table>
+        <tr>
+          <th>Requirement</th>
+          <th>Solution</th> 
+        </tr>
+        <tr>
+          <td>Design</td>
+          <td>PhotoShop, Figma, Canva, Adobe XD</td> 
+        </tr>
+        <tr>
+          <td>Front-end</td>
+          <td>React.js, Vue.js, Nuxt.js, HTML, CSS, JavaScript</td> 
+        </tr>
+        <tr>
+          <td>Back-end</td>
+          <td>Node.js, Express.js, Mongo, GraphQL</td> 
+        </tr>
+        <tr>
+          <td>ML</td>
+          <td>ML5, TensorFlow, Algorithm Design</td> 
+        </tr>
+        <tr>
+          <td>Shop</td>
+          <td>Stripe, PayFast, E-commerce, CMS, Mpesa, PesaPal</td> 
+        </tr>
+      </table>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius consequatur, libero, natus,
+       autem hic saepe exercitationem expedita alias corporis quisquam reiciendis ab quaerat
+        rerum itaque animi repudiandae dolor in fugit,
+       ratione inventore molestiae harum accusantium dignissimos. Id praesentium
+        quisquam asperiores ullam quaerat totam, voluptates esse vero repellendus amet, officiis inventore.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius consequatur, libero, natus,
+       autem hic saepe exercitationem expedita alias corporis quisquam reiciendis ab quaerat
+        rerum itaque animi repudiandae dolor in fugit,
+       ratione inventore molestiae harum accusantium dignissimos. Id praesentium
+        quisquam asperiores ullam quaerat totam, voluptates esse vero repellendus amet, officiis inventore.</p>
+  </div>
+      </div>
 </template>
