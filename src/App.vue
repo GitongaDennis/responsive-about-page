e43624b6f01006476e1f246cfda403c1848bbd80
Gitonga Dennis <gitonhadennis36.dg@gmail.com>
2019-12-04 10:37:09 +0300

    Desktop complete

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <div id="app">
+    <the-desktop-nav id="desktop-nav" />
+    <div class="view-container">
     <router-view />
   </div>
+  </div>
 </template>
+<script>
+import TheDesktopNav from '@/components/TheDesktopNav'
+//import TheDesktopNav from '@/components/TheDesktopNav'
+export default {
+  components: {
+    TheDesktopNav
+  }
+  
+}
+</script>
+
+<style lang="sass" scoped>
+.view-container
+  width: 79vw
+  padding: 1.5em
+  margin-left: 20vw
+  box-sizing: border-box
+  min-height: 100vh
+  </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,11 @@
 <template>
   <div class="about-card">
     <div class="card-title">
-      <img src="https://i.pinimg.com/originals/d3/69/d9/d369d9056795f553e244da66e8297cca.png" alt="avatar" class="avatar">
+      <img height="50px" src="https://i.pinimg.com/originals/d3/69/d9/d369d9056795f553e244da66e8297cca.png" alt="avatar" class="avatar">
       <div class="title-text-content">
     <div class="title-text-content">
-      <p>About Us </p>
-      <h2>Tour Kenya</h2>
+      <p>About Me </p>
+      <h2>Gitonga Dennis</h2>
     </div>
       </div>
   </div>
@@ -57,3 +57,50 @@
   </div>
       </div>
 </template>
+
+<style lang="sass" scoped>
+.about-card
+  min-height: calc(100vh - 150px)
+  background: #333
+  padding: 3em
+  border-radius: 10px
+  color: white
+  .card-title
+    display: flex
+    align-items: center
+    background: #222
+    border-radius: 10px
+    .avatar
+      height: 50px
+      border: 1px solid rgba(255, 255, 255, 0.4)
+      border-radius: 50%
+      margin: 0 30px
+  .title-text-content p
+    margin-bottom: 0
+    color: rgb(210, 210, 210)
+  .title-text-content h2
+    margin-top: 0
+
+.card-content p
+  line-height: 1.5
+  margin: 30px auto
+
+table 
+  border-spacing: 0px
+  margin: 50px auto
+  tr th, tr td
+    border: 1px solid white
+    border-top: none
+    padding: 10px
+    text-align: center
+    &:first-of-type
+      border-left: none
+    &:nth-of-type(2)
+      border-right: none
+
+@media screen and (max-width: 530px)
+  .about-card 
+    padding: 1em
+  .card-title h2
+    font-size: 1.3em
+</style>
